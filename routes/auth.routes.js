d938b7efd35d39d638be402fe0107210521a254d
AnaHerr <ana.herrera.91@gmail.com>
2022-02-14 13:44:43 +0100

    1st commit exercise

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const User = require('../models/User.model');
 const router = express.Router();
-const saltRound = 10;
+const saltRounds = 5;
 const bcrypt = require('bcrypt');
 
-router.get('/signup', (req, res, next) => {
+router.route('/signup', )
+.get((req, res, next) => {
 	res.render('signup');
+})
+.post((req,res)=>{
+	const username = req.body.username
+	const password = req.body.password
+
+    //Check the form is NOT empty. At work, please use a library
+	if(!username || !password){
+    res.render("signup", {errorMessage: "All fields are required"});
+    //return
+    }
+
+	User.findOne({username})
+	.then(user=>{
+		if(user && user.username)
+		{res.render("signup", {errorMessage: "User already taken!"})}
+
+		const salt = bcrypt.genSaltSync(saltRounds)
+		const hashedPwd = bcrypt.hashSync(password, salt)
+
+		User.create({username, password: hashedPwd})
+		.then(()=> {
+			res.redirect("/")
+		})
+	})
+	.catch(()=>{
+			res.redirect("/auth/signup")
+		})
 });
 
 router.get('/login', (req, res, next) => {
